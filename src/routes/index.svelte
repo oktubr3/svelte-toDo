8628233b94b1ad509d23b5aa1f3c7870763d8391
oktubr3 <mauroh@gmail.com>
2021-09-29 09:26:41 -0300

    2nd Commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- https://youtu.be/AV04qN6frmE?t=500 -->
+<script>
+import { each } from "svelte/internal";
+
+    let toDos = [
+        { task: "Comer", isComplete: false, createdAt: "2021-09-21"},
+        { task: "Cagar", isComplete: true, createdAt: "2021-09-21"},
+        { task: "Morir", isComplete: false, createdAt: "2021-09-21"}, 
+    ];
+</script>
+
+<ol>
+    {#each toDos as todo}
+    <li class:complete={todo.isComplete}>
+        {todo.task}        
+    </li> 
+    {/each}
+</ol>
+
+<style>
+    .complete {
+        text-decoration: line-through;
+    }
+</style>

e7f5cda8e3bd09f157fed848dd3ff9c88f46b657
oktubr3 <mauroh@gmail.com>
2021-09-30 19:36:52 -0300

    configurando firebase

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAGdyD6eJ8thwj0qYti-EcdrcDAuJ5I5VE",
+    authDomain: "todotasks-feeec.firebaseapp.com",
+    projectId: "todotasks-feeec",
+    storageBucket: "todotasks-feeec.appspot.com",
+    messagingSenderId: "632936352755",
+    appId: "1:632936352755:web:20807dc61bd6417890e086"
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,45 @@
-<!-- Youtube Video:  https://www.youtube.com/watch?v=Npc_MOXZKjM&list=PLm_Qt4aKpfKiGbdjaHdOpry6Neza0etxZ&index=4 -->
+<!-- Youtube Video:  https://youtu.be/G3DRgWzlAjg?list=PLm_Qt4aKpfKiGbdjaHdOpry6Neza0etxZ&t=716 -->
 <script>
-import { each } from "svelte/internal";
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore'
+import { firebaseConfig } from "$lib/firebaseConfig";
+import { browser } from "$app/env";
 
-    let todos = [];
-    let task = "";
+const app = browser && (getApps().length === 0 ? initializeApp(firebaseConfig) : getApp());
+const db = browser && getFirestore();
 
-    const addTodo = () => {
-        let todo =  { task: task, isComplete: false, createdAt: new Date()};
+console.log( app, db)
+
+let todos = [];
+let task = "";
+let error = "";
+
+const addTodo = () => {
+    let todo =  { task: task, isComplete: false, createdAt: new Date()};
+    
+    if (task != "") {
         todos = [todo, ...todos];
         task = "";
-    };
+        error = "";
+    } else {
+        error = "Task is Empty";
+    }
+};
 
-    const markTodoAsComplete = (index) => {
-        todos[index].isComplete = !todos[index].isComplete;
-    };
+const markTodoAsComplete = (index) => {
+    todos[index].isComplete = !todos[index].isComplete;
+};
 
-    const deleteTodo = (index) => {
-        let deleteItem = todos[index];
-        todos = todos.filter((todo) => todo != deleteItem);
-    };
+const deleteTodo = (index) => {
+    let deleteItem = todos[index];
+    todos = todos.filter((todo) => todo != deleteItem);
+};
 
-    const keyIsPressed = (event) => {
-        if (event.key === "Enter"){
-            addTodo();
-        }
-    };
+const keyIsPressed = (event) => {
+    if (event.key === "Enter"){
+        addTodo();
+    }
+};
 
 $: console.table(todos);
 </script>
@@ -47,10 +62,16 @@ $: console.table(todos);
     {:else}
         <p>No ToDos Found</p>
     {/each}
+    <p class="error">{error}</p>
 </ol>
+
+<svelte:window on:keydown={keyIsPressed} />
 
 <style>
     .complete {
         text-decoration: line-through;
+    }
+    .error {
+        color: red;
     }
 </style>
